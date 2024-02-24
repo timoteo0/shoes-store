a59b23cb110f77b6d5e085d2936b7fe42d5474b6
Timoteo Alves do Nascimento <timoteonascimento8@gmail.com>
2024-02-24 18:45:15 -0300

    feat: add initial brand card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { Header } from "./components/Header";
 import "./index.css";
 
 import offers from "./assets/ofertas.png";
+import { BrandCard } from "./components/Brand-card";
 
 function App() {
   return (
     <>
       <Header />
       <img className="w-full" src={offers} />
+
+      <div className="px-24 py-10 flex justify-center gap-10">
+        <BrandCard />
+      </div>
     </>
   );
 }
---
+++ src/components/Brand-card.tsx
@@ -0,0 +1,9 @@
+export function BrandCard() {
+  return (
+    <>
+      <button className="w-[198px] h-[50px] border-gray-dark border border-solid rounded-md">
+        <span>Adidas</span>
+      </button>
+    </>
+  );
+}
