5026758ba8370fbc5f6be04b784bbe13247552cd
Timoteo Alves do Nascimento <timoteonascimento8@gmail.com>
2024-02-24 20:11:51 -0300

    feat: add products

---
+++ src/products.ts
@@ -0,0 +1,107 @@
+import adidas1 from "./assets/1_adidas.png";
+import adidas2 from "./assets/2_adidas.png";
+import adidas3 from "./assets/3_adidas.png";
+import adidas4 from "./assets/3_adidas.png";
+
+import nike1 from "./assets/1_nike.png";
+import nike2 from "./assets/2_nike.png";
+import nike3 from "./assets/3_nike.png";
+import nike4 from "./assets/4_nike.png";
+
+import puma1 from "./assets/1_puma.png";
+import puma2 from "./assets/2_puma.png";
+
+import olympikus1 from "./assets/1_olympikus.png";
+import olympikus2 from "./assets/2_olympikus.png";
+
+const data = [
+  {
+    img: adidas1,
+    title: "Adidas Grand Court Base",
+    prevPrice: "R$140,00",
+    newPrice: "100",
+    company: "Adidas",
+  },
+  {
+    img: adidas2,
+    title: "Adidas Showtheway 2.0",
+    prevPrice: "R$140,00",
+    newPrice: "200",
+    company: "Adidas",
+  },
+  {
+    img: adidas3,
+    title: "Nike Rivalry Low",
+    prevPrice: "R$140,00",
+    newPrice: "70",
+    company: "Adidas",
+  },
+  {
+    img: adidas4,
+    title: "Adidas ADI2000",
+    prevPrice: "R$140,00",
+    newPrice: "290",
+    company: "Adidas",
+  },
+  {
+    img: nike1,
+    title: "Nike Air Max Alpha Trainer 5",
+    prevPrice: "R$140,00",
+    newPrice: "190",
+    company: "Nike",
+  },
+
+  {
+    img: nike2,
+    title: "Nike Pegasus 40",
+    prevPrice: "R$140,00",
+    newPrice: "280",
+    company: "Nike",
+  },
+  {
+    img: nike3,
+    title: "Nike Pegasus 40",
+    prevPrice: "R$140,00",
+    newPrice: "290",
+    company: "Nike",
+  },
+  {
+    img: nike4,
+    title: "Nike Pegasus 40",
+    prevPrice: "R$140,00",
+    newPrice: "250",
+    company: "Nike",
+  },
+
+  {
+    img: puma1,
+    title: "Puma RS-TRCK OTDR",
+    prevPrice: "R$140,00",
+    newPrice: "250",
+    company: "Puma",
+  },
+  {
+    img: puma2,
+    title: "Puma Velophasis Phased",
+    prevPrice: "R$140,00",
+    newPrice: "350",
+    company: "Puma",
+  },
+
+  {
+    img: olympikus1,
+    title: "Olympikus Challenger 4",
+    prevPrice: "R$140,00",
+    newPrice: "250",
+    company: "Olympikus",
+  },
+  {
+    img: olympikus2,
+    title: "Tênis Olympikus Citrus 2",
+    prevPrice: "R$140,00",
+    newPrice: "150",
+    company: "Olympikus",
+  },
+];
+
+export default data;
